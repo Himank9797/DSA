1f20771c2251ca9745acdaa19b315959205ef425
Himank9797 <himank0213@gmail.com>
2026-08-15 16:59:53 +0530

    Pattern priniting

---
+++ Patttern Prininting/Pattern1/pattern4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        for (int i = 0; i <= 5; i++)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+             cout << i << "";
+            }
+            cout << endl;
+        }
+        
+    return 0;
+}
+//PATTERN 4:
+// 0
+// 11
+// 222
+// 3333
+// 44444
+// 555555
---
+++ Patttern Prininting/Pattern3/Pattern3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 5 ; i++)
+    {
+        for (int j = 1; j < (6-i+1); j++)
+        {
+            cout << j ;
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
+// 12345
+// 1234
+// 123
+// 12
+// 1
